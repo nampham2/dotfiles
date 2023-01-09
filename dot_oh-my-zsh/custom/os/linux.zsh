f36e0933c343c7480b8101c85cbebe4bea4d09ce
Nam Pham <nam.pham@booking.com>
2023-01-09 14:25:15 +0100

    Enable proxy by default

--- dot_oh-my-zsh/custom/os/linux.zsh
+++ dot_oh-my-zsh/custom/os/linux.zsh
@@ -4,3 +4,11 @@ export TERM=xterm-256color
 # Custom paths
 export PATH="$PATH:$HOME/.local/bin"
 
+# Enable proxy by default
+PROXYADDR="http://webproxy.prod.booking.com:3128"
+export http_proxy="$PROXYADDR"
+export https_proxy="$PROXYADDR"
+export HTTP_PROXY="$PROXYADDR"
+export HTTPS_PROXY="$PROXYADDR"
+export no_proxy="localhost,127.0.0.1,booking.com"
+
